e8b8ee190e457352e71f83e49a1c2c1533be9adc
Felix Jahn <felix.jahn@outlook.com>
2024-09-09 16:39:31 +0200

    REFACTOR: Make use of `CanExecuteCommand` trait

--- src/SecretGenerators/LinuxSecretGenerator.php
+++ src/SecretGenerators/LinuxSecretGenerator.php
@@ -10,15 +10,17 @@ use function hash;
 
 final class LinuxSecretGenerator implements SecretGenerator
 {
+    use CanExecuteCommand;
+
     /** @throws SecretGenerationException If no suitable secret could be generated */
     public function generate(): string
     {
-        $cpuId = shell_exec('grep -m 1 \'Serial\' /proc/cpuinfo | awk \'{print $3}\'')
-            ?: shell_exec('cat /proc/cpuinfo | grep \'model name\' | head -1 | awk -F\': \' \'{print $2}\'')
+        $cpuId = $this->executeCommand('grep -m 1 \'Serial\' /proc/cpuinfo | awk \'{print $3}\'')
+            ?: $this->executeCommand('cat /proc/cpuinfo | grep \'model name\' | head -1 | awk -F\': \' \'{print $2}\'')
             ?: throw new SecretGenerationException('CPU ID could not be read');
 
-        $macAddress = shell_exec('ip link show | awk \'/ether/ {print $2}\' | head -n 1')
-            ?: shell_exec('ifconfig -a | grep -Po \'HWaddr \K.*$\' | head -n 1')
+        $macAddress = $this->executeCommand('ip link show 2>/dev/null | awk \'/ether/ {print $2}\' 2>/dev/null | head -n 1 2>/dev/null')
+            ?: $this->executeCommand('ifconfig -a | grep -Po \'HWaddr \K.*$\' | head -n 1')
             ?: throw new SecretGenerationException('MAC Address could not be read');
 
         if (!file_exists('/etc/machine-id')) {
